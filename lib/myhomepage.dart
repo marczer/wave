8e73c91903a2ec463b02e0622eaaa2547fd08e32
Clément Kouadio <clement.kouadio@intuitcorporate.com>
2023-01-11 16:17:48 +0000

    .

--- lib/myhomepage.dart
+++ lib/myhomepage.dart
@@ -7,22 +7,21 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-  // bool _obscureText = true;
-   bool _obscureSolde = true;
-   String solde = '1.500 F' ;
-
+// bool _obscureText = true;
+bool _obscureSolde = true;
+String solde = '1.500 F';
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: const Color.fromARGB(255, 71, 73, 205),
-        leading: const Icon(Icons.settings),
-      ),
-       body: SingleChildScrollView(
-        child: Column(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: const Color.fromARGB(255, 71, 73, 205),
+          leading: const Icon(Icons.settings),
+        ),
+        body: SingleChildScrollView(
+            child: Column(
           children: [
             Container(
               width: double.infinity,
@@ -31,116 +30,116 @@ class _MyHomePageState extends State<MyHomePage> {
               child: Stack(
                 children: [
                   Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    RichText(
-                      text:  TextSpan(
-                        text: solde,
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 30,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      RichText(
+                        text: TextSpan(
+                          text: solde,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 30,
+                          ),
                         ),
                       ),
-                    ),
-                    IconButton(
-                      onPressed: () {
-                        setState(() {
-                          if (_obscureSolde == true) {
-                          solde = '1.500 F';
-                          _obscureSolde = false;
-                        } else {
-                          solde = '•••••••••';
-                          _obscureSolde = true;
-                        }
-                        });
-                      },
-                     icon: Icon(
-                      _obscureSolde? Icons.visibility_off:Icons.visibility,
-                      color: Colors.white,
+                      IconButton(
+                        onPressed: () {
+                          setState(() {
+                            if (_obscureSolde == true) {
+                              solde = '1.500 F';
+                              _obscureSolde = false;
+                            } else {
+                              solde = '•••••••••';
+                              _obscureSolde = true;
+                            }
+                          });
+                        },
+                        icon: Icon(
+                          _obscureSolde
+                              ? Icons.visibility_off
+                              : Icons.visibility,
+                          color: Colors.white,
+                        ),
                       ),
+                      // const TransactionSection(),
+                    ],
+                  ),
+                  Positioned(
+                    left: 0,
+                    right: 0,
+                    top: 50,
+                    child: Container(
+                      alignment: Alignment.center,
+                      margin: const EdgeInsets.only(top: 80),
+                      height: 100,
+                      // width:  414,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.white,
+                      ),
+                      // child: Container(),
                     ),
-                  // const TransactionSection(),
-                   ],
-                    ),
-                     Positioned(
-                      left: 0,
-                      right: 0,
-                      top: 50,
+                  ),
+                  Positioned(
+                      right: 50,
+                      bottom: 10,
                       child: Container(
                         alignment: Alignment.center,
-                        margin: const EdgeInsets.only(top: 80),
-                        height: 100,
-                        // width:  414,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Colors.white,
+                        child: Image.asset(
+                          'lib/assets/wavecard.png',
+                          width: 300,
+                          height: 150,
                         ),
-                        // child: Container(),
-                      ),
-                    ),
-                     Positioned(
-                        right: 50,
-                        bottom: 10,
-                        child: Container(
-                          alignment: Alignment.center,
-                          child: Image.asset(
-                            'lib/assets/wavecard.png',
-                            width: 300,
-                            height: 150,
-                          ),
-                        )),
+                      )),
                 ],
               ),
             ),
             Container(
-              width: 250,
+              // width: 250,
+              color: Colors.white,
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   Column(
                     children: [
                       Container(
                           decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(100),
-                          color: const Color.fromARGB(255, 223, 230, 255),
+                            borderRadius: BorderRadius.circular(100),
+                            color: const Color.fromARGB(255, 223, 230, 255),
                           ),
                           width: 70,
                           height: 70,
                           margin: const EdgeInsets.only(bottom: 5),
                           padding: const EdgeInsets.all(15),
-                          child: Icon(Icons.person)
+                          child: const Icon(Icons.person)),
+                      const Text(
+                        'Transfert',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
                         ),
-                          const Text(
-                            'Transfert',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 15,
-                            ),
-                       )
+                      )
                     ],
                   ),
-
                   Column(
                     children: [
                       Container(
                           decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(100),
-                          color: const Color.fromARGB(255, 223, 230, 255),
+                            borderRadius: BorderRadius.circular(100),
+                            color: const Color.fromARGB(255, 223, 230, 255),
                           ),
                           width: 70,
                           height: 70,
                           margin: const EdgeInsets.only(bottom: 5),
                           padding: const EdgeInsets.all(15),
-                          child: Icon(Icons.shopping_bag)
+                          child: const Icon(Icons.shopping_bag)),
+                      const Text(
+                        'Paiements',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
                         ),
-                          const Text(
-                            'Paiements',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 15,
-                            ),
-                       )
+                      )
                     ],
                   ),
                 ],
@@ -149,119 +148,35 @@ class _MyHomePageState extends State<MyHomePage> {
             Container(
               color: const Color.fromARGB(255, 241, 241, 241),
               height: 10,
-                ),
-
-
-
+            ),
             Container(
               color: Colors.white,
               child: SingleChildScrollView(
                 child: Column(
-                  children: const [
-                    ListTile(
-                      title: Text(
-                        'Retrait',
-                        style: TextStyle(
-                          color: Color.fromARGB(255, 71, 73, 205),
-                          fontWeight: FontWeight.bold,
+                  children: List.filled(20, 0)
+                      .map(
+                        (e) => const ListTile(
+                          title: Text(
+                            'Retrait',
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 71, 73, 205),
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          subtitle: Text(
+                            'septembre 30',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          trailing: Text('- 5000F'),
                         ),
-                      ),
-                      subtitle: Text(
-                        'septembre 30',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      trailing: Text('- 5000F'),
+                      )
+                      .toList(),
                 ),
-                ListTile(
-                      title: Text(
-                        'Retrait',
-                        style: TextStyle(
-                          color: Color.fromARGB(255, 71, 73, 205),
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      subtitle: Text(
-                        'septembre 30',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      trailing: Text('- 5000F'),
-                ),
-                ListTile(
-                      title: Text(
-                        'Retrait',
-                        style: TextStyle(
-                          color: Color.fromARGB(255, 71, 73, 205),
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      subtitle: Text(
-                        'septembre 30',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      trailing: Text('- 5000F'),
-                ),
-                ListTile(
-                      title: Text(
-                        'Retrait',
-                        style: TextStyle(
-                          color: Color.fromARGB(255, 71, 73, 205),
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      subtitle: Text(
-                        'septembre 30',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      trailing: Text('- 5000F'),
-                ),
-                ListTile(
-                      title: Text(
-                        'Retrait',
-                        style: TextStyle(
-                          color: Color.fromARGB(255, 71, 73, 205),
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      subtitle: Text(
-                        'septembre 30',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      trailing: Text('- 5000F'),
-                ),
-                ListTile(
-                      title: Text(
-                        'Retrait',
-                        style: TextStyle(
-                          color: Color.fromARGB(255, 71, 73, 205),
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      subtitle: Text(
-                        'septembre 30',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      trailing: Text('- 5000F'),
-                ),
-               ]
-               )
-              )
-            )
-
+              ),
+            ),
           ],
-        )
-      )
-    );
+        )));
   }
 }
